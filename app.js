da6911f1750946d7528da2773313b68f64fda492
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-10-29 23:18:49 -0500

    Actualizando Algunas Secciones

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-/* Funcionalidad del icono de Hamburgesa */
-
-const ipad = window.matchMedia('screen and (max-width: 767px)');
 const menu = document.querySelector('.menu');
+const footer = document.getElementById('footer');
 const btnBurger = document.querySelector('#burger-menu');
+const ipad = window.matchMedia('screen and (max-width: 767px)');
 
 ipad.addListener(validation);
 
+document.addEventListener('DOMContentLoaded', footerDOM);
+
+/* Funcionalidad del icono de Hamburgesa */
 function validation(event) {
     if (event.matches) {
         btnBurger.addEventListener('click', hideShow);
@@ -19,4 +21,11 @@ function hideShow() {
     } else {
         menu.classList.add('is-active');
     }
+}
+
+function footerDOM() {
+    const year = new Date().getFullYear();
+    const name = 'Daniel Fernando Yepez Vèlez';
+
+    footer.textContent = `${name} - ${year}`;
 }
